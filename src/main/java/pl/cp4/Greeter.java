edacb9fe2e5ac7a31ecc65fffcba33ac297864e5
phantomhelper <dankaonpc@gmail.com>
2023-03-06 13:01:04 +0100

    Initial commit + start

---
+++ src/main/java/pl/cp4/Greeter.java
@@ -0,0 +1,9 @@
+package pl.cp4;
+
+public class Greeter {
+    public void greet(String name){
+
+        System.out.println(String.format("Hello %s", name));
+    }
+
+}
---
+++ src/main/java/pl/cp4/Main.java
@@ -0,0 +1,32 @@
+package pl.cp4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jakub", "Michal", "Agnieszka", "Ola", "Kasia");
+
+
+        Greeter greeter = new Greeter();
+        greeter.greet("Jakub"); // -> Hello Jakub
+
+        List<String> ladies = new ArrayList<String>();
+
+        for (String name: names){
+            if (name.endsWith("a")){
+                ladies.add(name);
+            }
+        }
+
+        for (String ladyName: ladies){
+            greeter.greet(ladyName);
+        }
+
+        names.stream()
+                .filter(name -> name.endsWith("a"))
+                .forEach(greeter::greet);
+
+    }
+}
